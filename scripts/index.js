ed8afd21eb9a19e5d71afe7c75f4953aeefcac68
kamille57 <126716411+kamille57@users.noreply.github.com>
2023-05-03 20:39:34 +0300

    fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,68 +1,26 @@
 let popup = document.querySelector('.popup');
 let buttonPopupOpen = document.querySelector('.profile-info__edit-button');
 let buttonPopupClose = document.querySelector('.popup__closed');
-let buttonSaveData = document.querySelector('.popup-form__save-button');
-let togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup_opened');
-
-
-buttonPopupClose.addEventListener('click', () => togglePopupState(popup));
-
 let popupForm = document.querySelector('.popup-form');
-let popupName = document.querySelector('.popup-item_name');
-let popupHobby = document.querySelector('.popup-item_hobby');
+let popupName = document.getElementById('name');
+let popupHobby = document.getElementById('hobby');
 let profileName = document.querySelector('.profile-info__name');
 let profileHobby = document.querySelector('.profile-info__profession');
+let togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup_opened');
 
-buttonPopupOpen.addEventListener('click', () => {
+function openPopup () {
     togglePopupState(popup);
     popupName.value = profileName.textContent; 
     popupHobby.value = profileHobby.textContent;
-});
+};
 
-popupForm.addEventListener('submit', fillForm);
 function fillForm(evt) {
     evt.preventDefault();
     profileName.textContent = popupName.value;
     profileHobby.textContent = popupHobby.value; 
     togglePopupState(popup);
-}
+};
 
-
-
-
-
-
-
-
-
-
-/*const buttonPopupOpen = document.querySelector('.profile__popup-open');
-const popup = document.querySelector('.popup');
-const buttonPopupClose = document.querySelector('.popup__closed');
-const togglePopupState = () => popup.classList.toggle('popup__opened');
-
-buttonPopupOpen.addEventListener('click', togglePopupState);
-buttonPopupClose.addEventListener('click', togglePopupState);
-
-
-popup.addEventListener('click', (evt) => {
-    if (evt.target  === evt.currentTarget) {
-        togglePopupState ()
-}
-});
-
-
-
-let popupForm = document.querySelector('.popup-form');
-let popupName = document.querySelector('.popup-item_name');
-let popupHobby = document.querySelector('.popup-item_hobby');
-let profileName = document.querySelector('.profile__name');
-let profileHobby = document.querySelector('.profile__profession');
-
+buttonPopupClose.addEventListener('click', () => togglePopupState(popup));
 popupForm.addEventListener('submit', fillForm);
-
-function fillForm() {
-    fillForm.preventDefault()
-    profileName.textContent = popupName.value;
-    profileHobby.textContent = popupHobby.value; 
-};*/
+buttonPopupOpen.addEventListener('click', (openPopup));
