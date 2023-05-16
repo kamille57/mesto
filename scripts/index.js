0e5bb3bc3898a82dbfa7f1340deb1823c4fe8f66
kamille57 <126716411+kamille57@users.noreply.github.com>
2023-05-16 15:05:46 +0300

    popup add form

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,19 @@
-let popup = document.querySelector('.popup');
+let popupEdit = document.querySelector('.popup_edit');
+let popupAdd = document.querySelector('.popup_add')
 let buttonPopupOpen = document.querySelector('.profile-info__edit-button');
-let buttonPopupClose = document.querySelector('.popup__closed');
+let buttonCloseEdit = document.getElementById('closeButtonEdit');
+let buttonCloseAdd = document.getElementById('closeButtonAdd');
 let popupForm = document.querySelector('.popup-form');
 let popupName = document.getElementById('name');
 let popupHobby = document.getElementById('hobby');
 let profileName = document.querySelector('.profile-info__name');
 let profileHobby = document.querySelector('.profile-info__profession');
 let togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup_opened');
+let imageAddButton = document.querySelector('.profile-info__add-button');
+let toggleLikeState = document.querySelector('.element__like');
 
-function openPopup () {
-    togglePopupState(popup);
+function openEditPopup () {
+    togglePopupState(popupEdit);
     popupName.value = profileName.textContent; 
     popupHobby.value = profileHobby.textContent;
 };
@@ -18,9 +22,17 @@ function fillForm(evt) {
     evt.preventDefault();
     profileName.textContent = popupName.value;
     profileHobby.textContent = popupHobby.value; 
-    togglePopupState(popup);
+    togglePopupState(popupEdit);
 };
 
-buttonPopupClose.addEventListener('click', () => togglePopupState(popup));
+toggleLikeState.addEventListener('click', function(evt) {
+    const eventTarget = evt.target;
+    eventTarget.setAttribute('disabled', true);
+}
+);
+
+imageAddButton.addEventListener('click', () => togglePopupState(popupAdd));
+buttonPopupOpen.addEventListener('click', openEditPopup);
+buttonCloseEdit.addEventListener('click', () => togglePopupState(popupEdit));
+buttonCloseAdd.addEventListener('click', () => togglePopupState(popupAdd));
 popupForm.addEventListener('submit', fillForm);
-buttonPopupOpen.addEventListener('click', (openPopup));
