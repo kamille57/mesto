2c8f93fb8ae7c20a707702dcef92c810631b2608
kamille57 <126716411+kamille57@users.noreply.github.com>
2023-05-19 00:32:53 +0300

    new js features added

--- scripts/index2.js
+++ scripts/index2.js
@@ -1,6 +1,7 @@
 let popupEdit = document.querySelector('.popup_edit');
 let popupAdd = document.querySelector('.popup_add');
 let buttonPopupOpen = document.querySelector('.profile-info__edit-button');
+let addImageOpen = document.querySelector('.profile-info__add-button');
 let buttonCloseEdit = document.getElementById('editClose');
 let buttonCloseAdd = document.getElementById('addClose');
 let popupFormEdit = document.getElementById('popupEdit');
@@ -10,9 +11,11 @@ let popupHobby = document.getElementById('hobby');
 let profileName = document.querySelector('.profile-info__name');
 let profileHobby = document.querySelector('.profile-info__profession');
 let togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup_opened');
-let addImageOpen = document.querySelector('.profile-info__add-button');
-const elementsContainer = document.querySelector('.elements');
-const figureTemplate = document.querySelector('#elements').content;
+
+
+
+
+
 const initialCards = [
     {
       name: 'Архыз',
@@ -39,26 +42,44 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
   ]; 
+  
 
-const elementsDescription = initialCards.map(function (item) {
-    return {
-       name: item.name,
-       link: item.link
-    };
-});
-
-function createImage () {
-   elementsDescription.forEach(addFigure);
-}
-
-function addFigure({name, link}) {
-    const figureElement = figureTemplate.querySelector('.element').cloneNode(true);
-    figureElement.querySelector('.element__pic').src = link;
-    figureElement.querySelector('.element__text').textContent = name;
-    elementsContainer.prepend(figureElement);
+  const cardsContainer = document.querySelector('.elements-container');
+  const cardsTemplate = document.querySelector('#template-cards').content;
+  const cardTemplate = cardsTemplate.querySelector('.element');
+  
+  
+function createElement (link, name) {    
+  const cardElement = cardTemplate.cloneNode(true); //вывод карточки из массива
+  console.log(cardElement);
+        cardElement.querySelector('.element__pic').src = link;
+        cardElement.querySelector('.element__text').textContent = name;
+        cardElement.querySelector('.element__pic').alt = name;
+        cardElement.querySelector('.element__like').addEventListener('click', function (evt) {   //ставим лайки
+          evt.target.classList.toggle('element__like_active');
+          });
+        cardElement.querySelector('.element__trash').addEventListener('click', function () {   //удаляем картинку
+          cardElement.remove();
+          });
+        return cardElement; 
 };
 
-createImage();
+initialCards.forEach((card) => {
+    const elementsDescription = createElement(card.link, card.name);
+          cardsContainer.append(elementsDescription);
+});
+
+const addCardField = document.getElementById('placeName');
+const addLinkField = document.getElementById('placeLink'); //инпут ввод линка
+const addCardButton = document.getElementById('addSaveButton'); //инпут ввод места
+  
+  addCardButton.addEventListener('click', newCard)
+  function newCard() {
+    const elementsDescription = createElement(addLinkField.value, addCardField.value);
+    cardsContainer.prepend(elementsDescription);
+    console.log(addCardField.value);
+    console.log(addLinkField.value);
+  }
 
 function openPopup () {
     togglePopupState(popupEdit);
@@ -74,9 +95,21 @@ function fillForm(evt) {
 };
 
 
+
+
+
+function fillAddForm(evt) {
+  evt.preventDefault();
+  addCardField.value = "";
+  addLinkField.value = "";
+  togglePopupState(popupAdd);
+};
+
 buttonPopupOpen.addEventListener('click', (openPopup));
 addImageOpen.addEventListener('click', () => togglePopupState(popupAdd));
 buttonCloseEdit.addEventListener('click', () => togglePopupState(popupEdit));
 buttonCloseAdd.addEventListener('click', () => togglePopupState(popupAdd));
 popupFormEdit.addEventListener('submit', fillForm);
-popupFormAdd.addEventListener('submit', () => togglePopupState(popupAdd));
+popupFormAdd.addEventListener('submit', fillAddForm);
+
+
